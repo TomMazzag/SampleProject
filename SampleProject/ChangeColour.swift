af045f10fb447123b9918ac0458a9ee61b20fed9
Thomas Mazzag <itstomazz@gmail.com>
2022-06-18 19:35:17 +0100

    Storyboards and icons

---
+++ SampleProject/ChangeColour.swift
@@ -0,0 +1,64 @@
+//
+//  ChangeColour.swift
+//  SampleProject
+//
+//  Created by Thomas Mazzag on 03/06/2022.
+//
+
+import UIKit
+
+class ChangeColour: UIViewController {
+    
+    
+    @IBOutlet weak var rVal: UISlider!
+    @IBOutlet weak var gVal: UISlider!
+    @IBOutlet weak var bVal: UISlider!
+    
+    
+    @IBAction func rSlider(_ sender: UISlider) {
+        print(Int((sender.value)))
+        Preview.backgroundColor = UIColor(red: CGFloat((rVal.value))/255, green: CGFloat((gVal.value))/255, blue: CGFloat((bVal.value))/255, alpha: 1)
+    }
+    @IBAction func gSlider(_ sender: UISlider) {
+        print(Int((sender.value)))
+        Preview.backgroundColor = UIColor(red: CGFloat((rVal.value))/255, green: CGFloat((gVal.value))/255, blue: CGFloat((bVal.value))/255, alpha: 1)
+    }
+    @IBAction func bSlider(_ sender: UISlider) {
+        print(Int((sender.value)))
+        Preview.backgroundColor = UIColor(red: CGFloat((rVal.value))/255, green: CGFloat((gVal.value))/255, blue: CGFloat((bVal.value))/255, alpha: 1)
+    }
+    
+
+    @IBOutlet weak var Preview: UILabel!
+    //Preview.layer.masksToBounds = true
+    //Preview.layer.cornerRadius = 5
+    
+    
+
+    
+    
+    
+    
+    @IBAction func Change(_ sender: UIButton) {
+        
+        self.view.backgroundColor = UIColor(red: CGFloat((rVal.value))/255, green: CGFloat((gVal.value))/255, blue: CGFloat((bVal.value))/255, alpha: 1)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Preview.layer.masksToBounds = true
+        Preview.layer.cornerRadius = 5
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+
+}
--- SampleProject/ViewController.swift
+++ SampleProject/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Next: UIButton!
+    @IBOutlet var butName: UILabel!
+    @IBOutlet weak var nameE: UITextField!
+    
+    var finalN: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Next.tintColor = .green
+        nameE.delegate = self
+        
     }
+    
+    @IBAction func Sub(_ sender: Any) {
+            
+        butName.text = (nameE.text)
+        finalN = (nameE.text!)
+        print (finalN)
+        
+        }
+    
 
-
+// Makes text box opened for name input hide when user clicks away
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameE.resignFirstResponder()
+    }
+    
 }
 
+// Makes text box opened for name input hide when user clicks return
+extension ViewController : UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
+
+//    func HiS() {
+//
+//        var savd = finalN
+//        print (savd)
+//    }
